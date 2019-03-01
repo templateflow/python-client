49c8556733a505c4b19bd9e34e28b27db990786d
oesteban <code@oscaresteban.es>
2019-03-01 11:48:27 -0800

    address @effigies' comments

--- templateflow/conf/__init__.py
+++ templateflow/conf/__init__.py
@@ -12,8 +12,6 @@ TF_GITHUB_SOURCE = 'https://github.com/templateflow/templateflow.git'
 if not TF_HOME.exists():
     try:
         from datalad.api import install
-        TF_HOME.parent.mkdir(exist_ok=True, parents=True)
-        install(path=str(TF_HOME), source=TF_GITHUB_SOURCE, recursive=True)
     except ImportError:
         raise RuntimeError("""\
 'TemplateFlow repository not found at path %s, and DataLad/git-annex are not \
@@ -21,6 +19,9 @@ installed.
 Please download TemplateFlow manually from \
 https://files.osf.io/v1/resources/ue5gx/providers/osfstorage/?zip= \
 and place all templates under the path indicated above.""" % TF_HOME)
+    else:
+        TF_HOME.parent.mkdir(exist_ok=True, parents=True)
+        install(path=str(TF_HOME), source=TF_GITHUB_SOURCE, recursive=True)
 
 TF_LAYOUT = Layout(
     TF_HOME, validate=False, config='templateflow',
