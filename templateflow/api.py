ea91f39002f9c4c04b8980132077730dfa04bd12
oesteban <code@oscaresteban.es>
2019-06-04 09:52:18 -0700

    fix(tests): Add one more template with PD average

--- templateflow/api.py
+++ templateflow/api.py
@@ -74,7 +74,7 @@ def templates(**kwargs):
     True
 
     >>> templates(suffix='PD')
-    ['MNI152Lin', 'MNI152NLin2009cAsym']
+    ['MNI152Lin', 'MNI152NLin2009cAsym', 'MNIPediatricAsym']
 
     """
     return sorted(TF_LAYOUT.get_templates(**kwargs))
