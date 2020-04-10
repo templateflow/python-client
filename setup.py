ad09549dfd9a33b0f07cb02b2140ccdd50cbc743
oesteban <code@oscaresteban.es>
2020-04-10 11:45:06 -0700

    fix: still working out the tag version for setuptools_scm

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ from setuptools.command.egg_info import egg_info
 # Give setuptools a hint to complain if it's too old a version
 # 30.3.0 allows us to put most metadata in setup.cfg
 # Should match pyproject.toml
-SETUP_REQUIRES = ["setuptools >= 42.0", "setuptools_scm >= 3.4", "toml"]
+SETUP_REQUIRES = [
+    "setuptools >= 42.0",
+    "setuptools_scm >= 3.4",
+    "setuptools_scm_git_archive",
+    "toml",
+]
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 
