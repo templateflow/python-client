a2c05a0755dbd43148cae659f3e43cb7f1615616
Oscar Esteban <code@oscaresteban.es>
2021-09-03 08:50:30 +0200

    fix #75: drop doi2bib and query doi.org directly

--- templateflow/tests/test_api.py
+++ templateflow/tests/test_api.py
@@ -1,3 +1,4 @@
+"""Test citations."""
 import pytest
 
 from .. import api
@@ -11,57 +12,54 @@ mni2009_urls = [
     "https://doi.org/10.1007/3-540-48714-X_16",
 ]
 
-mni2009_fbib = (
-    "@article{mni152nlin2009casym1,\n"
-    "\tdoi = {10.1016/j.neuroimage.2010.07.033},\n"
-    "\turl = {https://doi.org/10.1016%2Fj.neuroimage.2010.07.033},\n"
-    "\tyear = 2011,\n"
-    "\tmonth = {jan},\n"
-    "\tpublisher = {Elsevier {BV}},\n"
-    "\tvolume = {54},\n"
-    "\tnumber = {1},\n"
-    "\tpages = {313--327},\n"
-    "\tauthor = {Vladimir Fonov and Alan C. Evans and Kelly Botteron and C. "
-    "Robert Almli and Robert C. McKinstry and D. Louis Collins},\n"
-    "\ttitle = {Unbiased average age-appropriate atlases for pediatric studies},\n"
-    "\tjournal = {{NeuroImage}}\n}"
-)
+mni2009_fbib = """\
+@article{Fonov_2011,
+\tdoi = {10.1016/j.neuroimage.2010.07.033},
+\turl = {https://doi.org/10.1016%2Fj.neuroimage.2010.07.033},
+\tyear = 2011,
+\tmonth = {jan},
+\tpublisher = {Elsevier {BV}},
+\tvolume = {54},
+\tnumber = {1},
+\tpages = {313--327},
+\tauthor = {Vladimir Fonov and Alan C. Evans and Kelly Botteron and C. Robert \
+Almli and Robert C. McKinstry and D. Louis Collins},
+\ttitle = {Unbiased average age-appropriate atlases for pediatric studies},
+\tjournal = {{NeuroImage}}
+}"""
 
-mni2009_lbib = (
-    "@incollection{mni152nlin2009casym4,\n"
-    "\tdoi = {10.1007/3-540-48714-x_16},\n"
-    "\turl = {https://doi.org/10.1007%2F3-540-48714-x_16},\n"
-    "\tyear = 1999,\n"
-    "\tpublisher = {Springer Berlin Heidelberg},\n"
-    "\tpages = {210--223},\n"
-    "\tauthor = {D. Louis Collins and Alex P. Zijdenbos and Wim F. C. "
-    "Baar{\\'{e}} and Alan C. Evans},\n"
-    "\ttitle = {{ANIMAL}$\\mathplus${INSECT}: Improved Cortical Structure "
-    "Segmentation},\n"
-    "\tbooktitle = {Lecture Notes in Computer Science}\n}"
-)
+mni2009_lbib = """\
+@incollection{Collins_1999,
+\tdoi = {10.1007/3-540-48714-x_16},
+\turl = {https://doi.org/10.1007%2F3-540-48714-x_16},
+\tyear = 1999,
+\tpublisher = {Springer Berlin Heidelberg},
+\tpages = {210--223},
+\tauthor = {D. Louis Collins and Alex P. Zijdenbos and Wim F. C. Baar{\\'{e}} and Alan C. Evans},
+\ttitle = {{ANIMAL}$\\mathplus${INSECT}: Improved Cortical Structure Segmentation},
+\tbooktitle = {Lecture Notes in Computer Science}
+}"""
 
 fslr_urls = [
     "https://doi.org/10.1093/cercor/bhr291",
     "https://github.com/Washington-University/HCPpipelines/tree/master/global/templates",
 ]
 
-fslr_fbib = (
-    "@article{fslr1,\n"
-    "\tdoi = {10.1093/cercor/bhr291},\n"
-    "\turl = {https://doi.org/10.1093%2Fcercor%2Fbhr291},\n"
-    "\tyear = 2011,\n"
-    "\tmonth = {nov},\n"
-    "\tpublisher = {Oxford University Press ({OUP})},\n"
-    "\tvolume = {22},\n"
-    "\tnumber = {10},\n"
-    "\tpages = {2241--2262},\n"
-    "\tauthor = {D. C. Van Essen and M. F. Glasser and D. L. Dierker and J. "
-    "Harwell and T. Coalson},\n"
-    "\ttitle = {Parcellations and Hemispheric Asymmetries of Human Cerebral "
-    "Cortex Analyzed on Surface-Based Atlases},\n"
-    "\tjournal = {Cerebral Cortex}\n}"
-)
+fslr_fbib = """\
+@article{Van_Essen_2011,
+\tdoi = {10.1093/cercor/bhr291},
+\turl = {https://doi.org/10.1093%2Fcercor%2Fbhr291},
+\tyear = 2011,
+\tmonth = {nov},
+\tpublisher = {Oxford University Press ({OUP})},
+\tvolume = {22},
+\tnumber = {10},
+\tpages = {2241--2262},
+\tauthor = {D. C. Van Essen and M. F. Glasser and D. L. Dierker and J. Harwell and T. Coalson},
+\ttitle = {Parcellations and Hemispheric Asymmetries of Human Cerebral Cortex Analyzed on \
+Surface-Based Atlases},
+\tjournal = {Cerebral Cortex}
+}"""
 
 fslr_lbib = (
     "https://github.com/Washington-University/HCPpipelines/tree/master/global/templates"
@@ -77,11 +75,12 @@ fslr_lbib = (
     ],
 )
 def test_citations(tmp_path, template, urls, fbib, lbib):
+    """Check the correct composition of citations."""
     assert api.get_citations(template) == urls
     bibs = api.get_citations(template, bibtex=True)
     if bibs:
-        assert bibs[0] == fbib
-        assert bibs[-1] == lbib
+        assert "".join(bibs[0]) == fbib
+        assert "".join(bibs[-1]) == lbib
     else:
         # no citations currently
         assert template == "fsaverage"
