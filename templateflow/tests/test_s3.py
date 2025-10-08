aa13e0eeacf7d56e47d9e46a7d9c85f72410f7bb
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-10-08 08:01:43 -0400

    rf: Remove unnecessary global constants from _s3

--- templateflow/tests/test_s3.py
+++ templateflow/tests/test_s3.py
@@ -44,31 +44,36 @@ def test_get_skel_file(tmp_path, monkeypatch):
     # First execution, the S3 stub is created (or datalad install)
     reload(tfc)
 
-    local_md5 = tfc._s3.TF_SKEL_MD5
-    monkeypatch.setattr(tfc._s3, 'TF_SKEL_MD5', 'invent')
-    new_skel = tfc._s3._get_skeleton_file()
+    md5content = b'anything'
+
+    def mock_get(*args, **kwargs):
+        class MockResponse:
+            status_code = 200
+            ok = True
+            content = md5content
+
+        return MockResponse()
+
+    monkeypatch.setattr(requests, 'get', mock_get)
+
+    # Mismatching the local MD5 causes an update
+    new_skel = tfc._s3._get_skeleton_file(timeout=10)
     assert new_skel is not None
     assert Path(new_skel).exists()
-    assert Path(new_skel).stat().st_size > 0
+    assert Path(new_skel).read_bytes() == b'anything'
 
-    latest_md5 = (
-        requests.get(
-            tfc._s3.TF_SKEL_URL(release='master', ext='md5', allow_redirects=True), timeout=10
-        )
-        .content.decode()
-        .split()[0]
-    )
-    monkeypatch.setattr(tfc._s3, 'TF_SKEL_MD5', latest_md5)
-    assert tfc._s3._get_skeleton_file() is None
+    md5content = tfc._s3.load_data.readable('templateflow-skel.md5').read_bytes()
+    # Matching the local MD5 skips the update
+    assert tfc._s3._get_skeleton_file(timeout=10) is None
 
-    monkeypatch.setattr(tfc._s3, 'TF_SKEL_MD5', local_md5)
+    # Bad URL fails to update
     monkeypatch.setattr(tfc._s3, 'TF_SKEL_URL', 'http://weird/{release}/{ext}'.format)
-    assert tfc._s3._get_skeleton_file() is None
+    assert tfc._s3._get_skeleton_file(timeout=10) is None
 
     monkeypatch.setattr(
         tfc._s3, 'TF_SKEL_URL', tfc._s3.TF_SKEL_URL(release='{release}', ext='{ext}z').format
     )
-    assert tfc._s3._get_skeleton_file() is None
+    assert tfc._s3._get_skeleton_file(timeout=10) is None
 
 
 def test_update_s3(tmp_path, monkeypatch):
@@ -78,19 +83,19 @@ def test_update_s3(tmp_path, monkeypatch):
     monkeypatch.setenv('TEMPLATEFLOW_USE_DATALAD', 'off')
     monkeypatch.setenv('TEMPLATEFLOW_HOME', str(newhome))
 
-    assert tfc._s3.update(newhome)
-    assert not tfc._s3.update(newhome, overwrite=False)
+    assert tfc._s3.update(newhome, timeout=10)
+    assert not tfc._s3.update(newhome, overwrite=False, timeout=10)
     for p in (newhome / 'tpl-MNI152NLin6Sym').glob('*.nii.gz'):
         p.unlink()
-    assert tfc._s3.update(newhome, overwrite=False)
+    assert tfc._s3.update(newhome, overwrite=False, timeout=10)
 
     # This should cover the remote zip file fetching
-    monkeypatch.setattr(tfc._s3, 'TF_SKEL_MD5', 'invent')
-    assert tfc._s3.update(newhome, local=False)
-    assert not tfc._s3.update(newhome, local=False, overwrite=False)
+    # monkeypatch.setattr(tfc._s3, 'TF_SKEL_MD5', 'invent')
+    assert tfc._s3.update(newhome, local=False, timeout=10)
+    assert not tfc._s3.update(newhome, local=False, overwrite=False, timeout=10)
     for p in (newhome / 'tpl-MNI152NLin6Sym').glob('*.nii.gz'):
         p.unlink()
-    assert tfc._s3.update(newhome, local=False, overwrite=False)
+    assert tfc._s3.update(newhome, local=False, overwrite=False, timeout=10)
 
 
 def mock_get(*args, **kwargs):
